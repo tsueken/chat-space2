0975abb23234d3f742492bf822363a8b7e947763
tsueken <tsueken@gmail.com>
2020-03-03 17:52:10 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,10 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.12.0'
 # Capistranoのログの表示に利用する
-set :application, 'chat-space'
+set :application, 'chat-space2'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:tsueken/chat-space.git'
+set :repo_url,  'git@github.com:tsueken/chat-space2.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
